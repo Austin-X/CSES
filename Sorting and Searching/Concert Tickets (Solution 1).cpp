9b63e6477d60b7131fbdeef8bb519163aec8b9b1
Austin-X <32133198+Austin-X@users.noreply.github.com>
2021-05-22 12:41:42 -0400

    Create Concert Tickets (Solution 1).cpp

---
+++ Sorting and Searching/Concert Tickets (Solution 1).cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int n, m, x;
+  cin >> n >> m;
+  int customers[m];
+  std::map<int, int> map;
+  for (int i = 0; i < n; i ++) {
+    cin >> x;
+    if (map.count(x) == 1) map[x] ++;
+    else map.emplace(x, 1);
+  }
+  for (int i = 0; i < m; i ++) cin >> customers[i];
+
+
+  for (int i = 0; i < m; i ++) {
+    auto it = map.upper_bound(customers[i]);
+    if (it == map.begin()) cout << "-1\n";
+    else {
+      it --;
+      int val = it->first;
+      cout << val << "\n";
+      map[val] --;
+      if (map[val] == 0) map.erase(val);
+    } 
+  }
+
+  return 0;
+}
